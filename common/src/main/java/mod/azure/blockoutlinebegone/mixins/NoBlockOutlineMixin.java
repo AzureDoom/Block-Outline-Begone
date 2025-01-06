dc06c235b3c7091ffdfecd4034eead15711a7a37
mooncatcher <lightoff2000@gmail.com>
2025-01-06 05:20:50 +0200

    Actual update
    Updated for the new renderBlockOutlines method

--- common/src/main/java/mod/azure/blockoutlinebegone/mixins/NoBlockOutlineMixin.java
+++ common/src/main/java/mod/azure/blockoutlinebegone/mixins/NoBlockOutlineMixin.java
@@ -1,12 +1,10 @@
 package mod.azure.blockoutlinebegone.mixins;
 
 import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.blaze3d.vertex.VertexConsumer;
 import mod.azure.blockoutlinebegone.CommonMod;
 import net.minecraft.client.renderer.LevelRenderer;
-import net.minecraft.core.BlockPos;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.Camera;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -14,10 +12,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(LevelRenderer.class)
 public class NoBlockOutlineMixin {
-
-    @Inject(method = "renderHitOutline", at = @At("HEAD"), cancellable = true)
-    public void onRenderHitOutline(PoseStack poseStack, VertexConsumer vertexConsumer, Entity entity, double camX, double camY, double camZ, BlockPos blockPos, BlockState blockState, CallbackInfo ci) {
-        if(CommonMod.disableOutline)
+    @Inject(method = "renderBlockOutline", at = @At("HEAD"), cancellable = true)
+    public void onRenderBlockOutline(Camera camera, MultiBufferSource.BufferSource bufferSource, PoseStack poseStack, boolean sort, CallbackInfo ci) {
+        if (CommonMod.disableOutline) {
             ci.cancel();
+        }
     }
 }
